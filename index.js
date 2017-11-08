465d5953d210bbc8bcb668c4a2fb191dcd17aa19
Andrius Skerla <andrius.skerla@gmail.com>
2017-11-08 17:50:24 +0000

    return in ms not in seconds

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function measure() {
     const timeStart = process.hrtime();
     return () => {
         const timeDiff = process.hrtime(timeStart);
-        return timeDiff[0] + (timeDiff[1] / 1e9);
+        return (timeDiff[0] + (timeDiff[1] / 1e9)) / 1e3;
     };
 }
 exports.measure = measure;
